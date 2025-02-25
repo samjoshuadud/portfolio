c7fe29e34d1bd1ea07a0336ca111643da17aa5d6
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2025-02-25 14:00:13 +0800

    Change Pic

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,11 @@ import "./globals.css";
 export const metadata = {
   title: "Portfolio",
   description: "E-Portfolio",
+ icons: {
+    icon: "/batman.png",
+    shortcut: "/batman.png",
+    apple: "/batman.png",
+  },
 };
 
 export default function RootLayout({ children }) {
